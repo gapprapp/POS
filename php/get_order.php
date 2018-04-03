bb9822d324b2819b2dbdcbcdcafe690687a8f587
mewole <37533052+mewole@users.noreply.github.com>
2018-04-03 13:37:27 +0700

    test

--- php/get_order.php
+++ php/get_order.php
@@ -1,11 +1,15 @@
 <?php
 	$conn = mysqli_connect("localhost", "root", "pkl2468GG", "pos");
-	$date  = $_POST['date']; 
+  $date  = $_POST['date'];
+  $cus_id = $_POST['cus_id'];
+  $b_id = $_POST['b_id'];
+  $number = $_POST['number'];
+  $user_id = $_POST['user_id'];
+  $pay = $_POST['pay'];
     
-    $sql = "
-    SELECT s.order_id,s.sum_price,c.customer_name FROM sale_order s INNER JOIN customer c 
-    ON s.customer_id = c.customer_id  WHERE date_time between '$date' and '$date 23:59:59'";
-    $result = mysqli_query($conn, $sql);
+  $sql = "SELECT s.order_id,s.total_price,s.date_time,c.customer_name FROM sale_order s INNER JOIN customer c 
+  ON s.customer_id = c.customer_id  WHERE date_time between '$date' and '$date 23:59:59'";
+  $result = mysqli_query($conn, $sql);
 
 	if(mysqli_num_rows($result) > 0){    
       while($row = mysqli_fetch_array($result)){      
