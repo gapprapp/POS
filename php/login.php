703068f4b4c4e4828a149e10d091edf3a40612af
mewole <37533052+mewole@users.noreply.github.com>
2018-04-07 12:52:27 +0700

    success

--- php/login.php
+++ php/login.php
@@ -4,13 +4,14 @@
 	$pass = $_POST['pass'];	
     
 	// Check pass match 
-	$sql_pass = "SELECT user_id,pwd FROM user WHERE user_name = '$name'";
+	$sql_pass = "SELECT user_id,pwd,role FROM user WHERE user_name = '$name'";
 	$res_pass = mysqli_query($conn, $sql_pass);
 
 	if(mysqli_num_rows($res_pass) > 0){
 		while($row = mysqli_fetch_array($res_pass)){
 			if(password_verify($pass, $row['pwd'])){
-				echo $row['user_id'];
+				$output[] = $row;
+				echo json_encode($output);
 			}else{
 				echo "fail";
 			}		
