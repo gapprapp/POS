8b6a2a995ea995a0d88ec3b972b36ad5b6e53a9a
mewole <37533052+mewole@users.noreply.github.com>
2018-04-09 17:49:38 +0700

    ้ีแก้ sale

--- php/check_detail_cus.php
+++ php/check_detail_cus.php
@@ -11,14 +11,22 @@
                $output[] = $row;                    
         }		
     }   
-    
-    $sql = "SELECT prod_price FROM detail_customer WHERE prod_id = '$prod_id' AND amt_threshold = '$amt'";
-    $result = mysqli_query($conn, $sql);
-    if(mysqli_num_rows($result) > 0){
-        while($row = mysqli_fetch_array($result)){
-              array_push($output,$row);                    
-        }		
-    }    
+
+    $sql = "SELECT max(amt_threshold) AS max_amt FROM detail_customer WHERE prod_id = '$prod_id' AND amt_threshold <= '$amt'";
+	$result = mysqli_query($conn, $sql);
+
+	if(mysqli_num_rows($result) > 0){
+		while($row = mysqli_fetch_array($result)){
+			$amount = $row['max_amt'];			
+			$sql = "SELECT prod_price FROM detail_customer WHERE prod_id = '$prod_id' AND amt_threshold = '$amount'";
+			$result1 = mysqli_query($conn, $sql);
+			if(mysqli_num_rows($result1) > 0){
+				while($row1 = mysqli_fetch_array($result1)){					
+                    array_push($output,$row1);  
+				}		
+			}					
+		}		
+	}  
 
     if($result){
         echo json_encode($output);		   
