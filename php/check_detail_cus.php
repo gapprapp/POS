db21f90c010fcbdab690a88f8df33085c6c5827a
mewole <37533052+mewole@users.noreply.github.com>
2018-04-09 20:55:21 +0700

    สักที

--- php/check_detail_cus.php
+++ php/check_detail_cus.php
@@ -1,37 +1,16 @@
 <?php
     $conn = mysqli_connect("localhost", "root", "pkl2468GG", "pos"); 
-    $cus_id = $_POST['cus_id'];   
-    $amt = $_POST['amt'];   
+    $cus_id = $_POST['cus_id'];  
     $prod_id = $_POST['prod_id'];   
  
     $sql = "SELECT special_prod_price FROM detail_customer WHERE prod_id = '$prod_id' AND customer_id = '$cus_id'";
     $result = mysqli_query($conn, $sql);
     if(mysqli_num_rows($result) > 0){
         while($row = mysqli_fetch_array($result)){
-               $output[] = $row;                    
+            echo $row['special_prod_price'];             
         }		
-    }   
-
-    $sql = "SELECT max(amt_threshold) AS max_amt FROM detail_customer WHERE prod_id = '$prod_id' AND amt_threshold <= '$amt'";
-	$result = mysqli_query($conn, $sql);
-
-	if(mysqli_num_rows($result) > 0){
-		while($row = mysqli_fetch_array($result)){
-			$amount = $row['max_amt'];			
-			$sql = "SELECT prod_price FROM detail_customer WHERE prod_id = '$prod_id' AND amt_threshold = '$amount'";
-			$result1 = mysqli_query($conn, $sql);
-			if(mysqli_num_rows($result1) > 0){
-				while($row1 = mysqli_fetch_array($result1)){					
-                    array_push($output,$row1);  
-				}		
-			}					
-		}		
-	}  
-
-    if($result){
-        echo json_encode($output);		   
     }else{
         echo "fail";
-    }
+    }  
     mysqli_close($conn);
 ?>
