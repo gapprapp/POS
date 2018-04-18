bed52c394ab09308d0ebfc04db5ff1651bd8b792
mewole <37533052+mewole@users.noreply.github.com>
2018-04-18 22:39:19 +0700

    5555

--- php/clone_search.php
+++ php/clone_search.php
@@ -1,13 +1,25 @@
 <?php
     $conn = mysqli_connect("localhost", "root", "pkl2468GG", "pos"); 
-  
-    $result = $mysql->query("select * from product");
-    $rows = array();
-    while($row = $result->fetch_array(MYSQL_ASSOC)) {
-        $rows[] = array_map("utf8_encode", $row);
+    $data = $_POST['term'];   
+      
+    $query = "SELECT p.prod_id,p.prod_name,p.barcode,p.prod_price,u.unit_name,p.img_string,p.prod_cost 
+        FROM product p INNER JOIN unit u ON p.unit_id = u.unit_id
+        WHERE p.prod_name LIKE '%$data%' OR p.barcode LIKE '%$data%'";
+    $result = mysqli_query($conn, $query);
+
+    if(mysqli_num_rows($result) > 0){    
+      while($row = mysqli_fetch_array($result)){          
+        $output[] = $row;
+      }   
+    }else{    
+      $output = array('prod_name' => 'Not Found');
+    }   
+
+    if($result){
+        echo json_encode($output);		   
+    }else{
+        echo "fail";
     }
-    echo json_encode($rows);
-    $result->close();
-    $mysql->close();
+    mysqli_close($conn);
 
 ?>
