9a7947cdc6cd1111d117fc290ad6b620db957168
mewole <37533052+mewole@users.noreply.github.com>
2018-04-19 20:54:04 +0700

    test

--- php/clone_search.php
+++ php/clone_search.php
@@ -1,14 +1,28 @@
 <?php
     $conn = mysqli_connect("localhost", "root", "pkl2468GG", "pos"); 
-    $data = $_GET['q'];   
+    $data = $_POST['phrase'];   
       
     $query = "SELECT p.prod_id,p.prod_name,p.barcode,p.prod_price,u.unit_name,p.img_string,p.prod_cost 
         FROM product p INNER JOIN unit u ON p.unit_id = u.unit_id
         WHERE p.prod_name LIKE '%$data%' OR p.barcode LIKE '%$data%'";
+
     $result = mysqli_query($conn, $query);
 
     if(mysqli_num_rows($result) > 0){    
-      while($row = mysqli_fetch_array($result)){          
+      while($row = mysqli_fetch_array($result)){ 
+        $old = array('old_price' => $row['prod_price']);
+        array_push($row,$old);
+        if(isset($_GET['cus_id'])){
+            $prod_id = $row['prod_id'];
+            $cus_id = $_GET['cus_id'];
+            $sql = "SELECT special_prod_price FROM detail_customer WHERE prod_id = '$prod_id' AND customer_id = '$cus_id'";
+            $result1 = mysqli_query($conn, $sql);
+            if(mysqli_num_rows($result1) > 0){
+                while($row1 = mysqli_fetch_array($result1)){
+                     $row['prod_price'] = $row1['special_prod_price'];                    
+                }		
+            }
+        }       
         $output[] = $row;
       }   
     }else{    
@@ -20,8 +34,6 @@
     }else{
         echo "fail";
     }
-    /*$age = array("Peter"=>"$data", "Ben"=>"37", "Joe"=>"43");
-    echo  json_encode($age);*/
     mysqli_close($conn);
 
 ?>
