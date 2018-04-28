c6553efa48fbd15b0b034209c7730833d705f4af
mewole <37533052+mewole@users.noreply.github.com>
2018-04-28 10:11:07 +0700

    aah

--- php/get_order_item.php
+++ php/get_order_item.php
@@ -4,7 +4,7 @@
   
 
   $sql = "SELECT b.branch_id,b.branch_name,o.order_number,o.date_time,u.user_name,c.customer_name,o.sum_price,o.total_discount
-  ,o.total_price,o.get_money,o.change_money FROM sale_order o INNER JOIN branch b ON o.branch_id = b.branch_id 
+  ,o.total_price,o.get_money,o.change_money,o.payment_type FROM sale_order o INNER JOIN branch b ON o.branch_id = b.branch_id 
   INNER JOIN user u ON o.user_id = u.user_id INNER JOIN customer c ON o.customer_id = c.customer_id
   WHERE o.order_id = '$order_id'";
   $result = mysqli_query($conn, $sql);
@@ -15,8 +15,8 @@
     }   
   } 
     
-  $sql = "SELECT i.item_id,i.prod_id,p.prod_name,i.prod_price,i.prod_amount FROM sale_order_item i INNER JOIN product p 
-  ON i.prod_id = p.prod_id WHERE i.order_id = '$order_id'";
+  $sql = "SELECT i.item_id,i.prod_id,p.prod_name,i.prod_price,i.prod_amount,u.unit_name FROM sale_order_item i INNER JOIN product p 
+  ON i.prod_id = p.prod_id INNER JOIN unit u ON p.unit_id = u.unit_id WHERE i.order_id = '$order_id'";
   $result = mysqli_query($conn, $sql);
 
 	if(mysqli_num_rows($result) > 0){    
