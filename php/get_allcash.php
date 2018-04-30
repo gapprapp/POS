27139e4b27d3edc0cf76db88d1e1a41e8201c0d4
mewole <37533052+mewole@users.noreply.github.com>
2018-04-30 17:07:12 +0700

    ye

--- php/get_allcash.php
+++ php/get_allcash.php
@@ -11,11 +11,11 @@
 		if($date || $user_name || $b_id){		
 			$sql = "SELECT u.user_name,b.branch_name,c.date_time,c.cash FROM cash_record c INNER JOIN user u 
             ON c.record_by = u.user_id INNER JOIN branch b ON c.branch_id = b.branch_id WHERE c.branch_id = '$b_id'             
-	        AND u.user_name LIKE '%$user_name%' AND c.date_time LIKE '%$date%'";
+	        AND u.user_name LIKE '%$user_name%' AND c.date_time LIKE '%$date%' LIMIT $start,100";
 		}else
 		{
 			$sql = "SELECT u.user_name,b.branch_name,c.date_time,c.cash FROM cash_record c INNER JOIN user u 
-            ON c.record_by = u.user_id INNER JOIN branch b ON c.branch_id = b.branch_id";			
+            ON c.record_by = u.user_id INNER JOIN branch b ON c.branch_id = b.branch_id LIMIT $start,100";			
 		}
 		
 	}else if($sort_by != "none" && $order != "none"){
@@ -23,31 +23,37 @@
 			if($order == "asc"){
                 $sql = "SELECT u.user_name,b.branch_name,c.date_time,c.cash FROM cash_record c INNER JOIN user u 
                 ON c.record_by = u.user_id INNER JOIN branch b ON c.branch_id = b.branch_id WHERE c.branch_id = '$b_id'             
-                AND u.user_name LIKE '%$user_name%' AND c.date_time LIKE '%$date%' ORDER BY c.$sort_by ASC";
+                AND u.user_name LIKE '%$user_name%' AND c.date_time LIKE '%$date%' ORDER BY c.$sort_by ASC LIMIT $start,100";
 			}else if($order == "desc"){
                 $sql = "SELECT u.user_name,b.branch_name,c.date_time,c.cash FROM cash_record c INNER JOIN user u 
                 ON c.record_by = u.user_id INNER JOIN branch b ON c.branch_id = b.branch_id WHERE c.branch_id = '$b_id'             
-                AND u.user_name LIKE '%$user_name%' AND c.date_time LIKE '%$date%' ORDER BY c.$sort_by DESC";
+                AND u.user_name LIKE '%$user_name%' AND c.date_time LIKE '%$date%' ORDER BY c.$sort_by DESC LIMIT $start,100";
 			}
 		}else{
 			if($order == "asc"){
                 $sql = "SELECT u.user_name,b.branch_name,c.date_time,c.cash FROM cash_record c INNER JOIN user u 
                 ON c.record_by = u.user_id INNER JOIN branch b ON c.branch_id = b.branch_id ORDER BY c.$sort_by 
-                ASC";
+                ASC LIMIT $start,100";
 			}else if($order == "desc"){
 				$sql = "SELECT u.user_name,b.branch_name,c.date_time,c.cash FROM cash_record c INNER JOIN user u 
                 ON c.record_by = u.user_id INNER JOIN branch b ON c.branch_id = b.branch_id ORDER BY c.$sort_by
-                 DESC";
+                 DESC LIMIT $start,100";
 			}
 		}
 	}
     $result = mysqli_query($conn, $sql);
-	$number_cash = mysqli_num_rows($result);
+
+	if(mysqli_num_rows($result) > 0){    
+      while($row = mysqli_fetch_array($result)){      
+        $output[] = $row;
+      }   
+  }  
 
 	if($result){
-		echo $number_cash;		   
+		echo json_encode($output);
 	}else{
 		echo "fail";
 	}
+	
 	mysqli_close($conn);
 ?>
